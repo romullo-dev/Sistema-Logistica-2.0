f43989211bd5cd40653a6b62e5a66f476ced1c38
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-03-26 21:36:52 -0300

    Commit

--- conexao/Conexao.php
+++ conexao/Conexao.php
@@ -1,24 +1,19 @@
 <?php
 
-class Conexao {
+class Conexao
+{
+    private $host = 'localhost';
+    private $database = 'db_sistema_logistica';
+    private $username = 'root';
+    private $password = '';                         
 
-    public function conectar ()  
+    public function conectar()
     {
-        $host = 'localhost';
-        $database = 'db_logistica';
-        $username = 'root';
-        $password = '';
-
         try {
-            $sql = new PDO('mysql:host=localhost;dbname=db_logistica;$username, $password ');
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->database}","{$this->username}","{$this->password}");
         } catch (PDOException $e) {
-            echo 'Erro'. e;
+            return false; 
         }
-
-
     }
-
-
-
-
 }
+
